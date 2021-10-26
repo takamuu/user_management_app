8710966a4ad801b0ac7f8fca8b735f23a25bc15f
takamuu <mr.takamuu@gmail.com>
2021-10-26 10:04:47 +0900

    Login function implementation

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,43 @@
-import { memo, VFC } from 'react';
+import { ChangeEvent, memo, useState, VFC } from 'react';
+import { Button } from '@chakra-ui/button';
+import { Input } from '@chakra-ui/input';
+import { Box, Divider, Flex, Heading, Stack } from '@chakra-ui/layout';
+
+import { useAuth } from '../../hooks/useAuth';
+import { PrymaryButton } from '../atoms/button/PrimaryButton';
 
 export const Login: VFC = memo(() => {
-    return <p>ログインページです</p>;
+    const { login, loading } = useAuth();
+
+    const [userId, setUserId] = useState('');
+
+    const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) =>
+        setUserId(e.target.value);
+
+    const onClickLogin = () => login(userId);
+
+    return (
+        <Flex align="center" justify="center" height="100vh">
+            <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+                <Heading as="h1" size="lg" textAlign="center">
+                    ユーザー管理アプリ
+                </Heading>
+                <Divider my={4} />
+                <Stack spacing={6} py={4} px={10}>
+                    <Input
+                        placeholder="ユーザーID"
+                        value={userId}
+                        onChange={onChangeUserId}
+                    />
+                    <PrymaryButton
+                        disabled={userId === ''}
+                        loading={loading}
+                        onClick={onClickLogin}
+                    >
+                        ログイン
+                    </PrymaryButton>
+                </Stack>
+            </Box>
+        </Flex>
+    );
 });
